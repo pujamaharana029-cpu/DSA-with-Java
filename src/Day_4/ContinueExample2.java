2bcb72dfd5c781cd6c56d9a177c483cfee17be05
Puja_Maharana <pujamaharana029@gmail.com>
2026-08-19 23:52:39 +0530

    Day 6 -while loop,break and continue practice question

---
+++ src/Day_4/ContinueExample2.java
@@ -0,0 +1,12 @@
+package Day_4;
+
+public class ContinueExample2 {
+    public static void main(String[] args) {
+        for (int i=1;i<=100;i++){
+            if(i%5==0){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}

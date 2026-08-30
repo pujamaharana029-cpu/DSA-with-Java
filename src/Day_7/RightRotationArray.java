1e488c08147e699de779efdaf45ae05c04d79e58
Puja_Maharana <pujamaharana029@gmail.com>
2026-08-30 23:16:19 +0530

    Day 16-Left and Right Rotation of Array

--- src/Day_7/RightRotationArray.java
+++ src/Day_7/RightRotationArray.java
@@ -2,17 +2,17 @@ package Day_7;
 
 import java.util.Scanner;
 
-public class RotateArray { //left rotation
+public class RightRotationArray {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int[] arr = {3, 14, 25, 65, 37, 98, 56};
         int n =sc.nextInt();   // no.of rotations takes place
         for (int i = 0; i < n; i++) {
-            int first = arr[0], j;    //Save first
-            for (j = 0; j < arr.length-1; j++) {
-                arr[j] = arr[j +1];     //shift elements
+            int last = arr[arr.length-1], j;    //Save last
+            for (j = arr.length-1; j>0; j--) {
+                arr[j] = arr[j-1];     //shift elements
             }
-            arr[arr.length -1] = first; // put first at End
+            arr[0] = last; // put last at beginning
         }
         for (int i = 0; i < arr.length; i++) { // repeating rotation n times.
             System.out.print(arr[i] + " ");
